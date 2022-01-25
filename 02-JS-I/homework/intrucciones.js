b71b7999e6913b8bbbb6c1321bbe89a905050489
Aaron <sistemasnoagroup@gmail.com>
2022-01-25 18:11:44 -0300

    Homework JS01

---
+++ 02-JS-I/homework/intrucciones.js
@@ -0,0 +1,5 @@
+*Una variable es como un contenedor donde podemos guardar cualquier tipo de cosas
+*Un string es una cadena de letras y numeros
+*Una funcion es una pequeña parte de un programa que cumple ciertas acciones particulares para la que fueron hechas.
+*Una declaracion if es una estructura que nos permite controlar y comparar partes que necesitemos.
+*Un valor booleano es un tipo de dato que solo puede tomar dos valores , verdadero o falso.
