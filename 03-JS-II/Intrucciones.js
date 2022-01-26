e3009f9685dd9b21a02d017d2117de17700c6a1b
Aaron <sistemasnoagroup@gmail.com>
2022-01-26 09:34:17 -0300

    Homework JS2 Done

---
+++ 03-JS-II/Intrucciones.js
@@ -0,0 +1,4 @@
+*For es un ciclo que nos ayuda a completar una tarea mediante iteracciones, una vez dada cierta condiccion el ciclo se completa y continua con la ejecucion del programa en cuestion.
+*Los simbolos && utilizados como operadores logicos para comparar una condiccion y otra.
+*Los simbolos || son utilizados como operadores para saber si se cumpleta una condicion U otra.
+*El simbolo ! es utilizado como negacion para negar (valga la redundacia) algo en una condicion.
